fa24bb019967cc17b96ca094eecad8f765d46684
Julie Rivest <julierivest93@gmail.com>
2017-12-30 15:46:05 -0500

    fixed omniauth facebook bugs

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if !@user.persisted?
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      @user.save
+      @user.save(validate:false)
     end
 
     sign_in_and_redirect @user, :event => :authentication
